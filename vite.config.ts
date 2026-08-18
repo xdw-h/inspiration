32e3aa69d06928cc45e830067817eb46339bb56e
xuedawei <xuedawei@mastercom.cn>
2026-08-18 18:11:15 +0800

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  base: process.env.VITE_BASE || '/',
   plugins: [vue()],
   build: { manifest: true },
   test: { environment: 'jsdom', include: ['tests/unit/**/*.spec.ts'] },
